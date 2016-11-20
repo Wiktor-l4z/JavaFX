ed53398debf13a8ad30473ceb84d491d977ae3e4
Wiktor <wiktorkalinowski@gmail.com>
2016-11-20 18:39:37 +0100

    Login From SQLite Database
    Login From SQLite Database

---
+++ Login From SQLite Database/src/sample/Controller.java
@@ -0,0 +1,63 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Label;
+
+
+import java.io.IOException;
+import java.sql.*;
+
+
+
+
+public class Controller {
+
+
+    @FXML
+    private Label lblMessage;
+    @FXML
+    private PasswordField txtPassword;
+    @FXML
+    private TextField txtUsername;
+
+    PreparedStatement pst = null;
+    ResultSet rs = null;
+    Connection conn;
+
+    @FXML
+    private void btnLoginAction() throws IOException {
+
+        try {
+            String query = "select * from UserDatabase where Username=? Password=?";
+
+            pst = conn.prepareStatement(query);
+            pst.setString(1, txtUsername.getText());
+            pst.setString(2, txtPassword.getText());
+            rs = pst.executeQuery();
+
+            if (rs.next()) {
+                lblMessage.setText("Login Successful");
+            } else {
+                lblMessage.setText("Username or Password invalid!");
+            }
+            pst.close();
+            rs.close();
+        } catch (Exception e1) {
+            lblMessage.setText("SQL Error");
+            System.err.println(e1);
+        }
+    }
+    public static Connection DbConnector() {
+        try {
+            Connection conn = null;
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:UserData.sqlite");
+            return conn;
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+}
---
+++ Login From SQLite Database/src/sample/Main.java
@@ -0,0 +1,47 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.sqlite.SQLiteConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class Main extends Application {
+    Connection conn;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        PreparedStatement pst = null;
+
+        Parent root = FXMLLoader.load(getClass().getResource("login1.fxml"));
+        primaryStage.setTitle("Login From SQLite Database ");
+        primaryStage.setScene(new Scene(root, 255, 200));
+        primaryStage.show();
+        CheckConnection();
+
+    }
+
+
+
+    public void CheckConnection() {
+     conn = Controller.DbConnector();
+         if ( conn == null) {
+            System.out.println("Connection Not Successful");
+            System.exit(1);
+        } else {
+            System.out.println("Connection Successful");
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
