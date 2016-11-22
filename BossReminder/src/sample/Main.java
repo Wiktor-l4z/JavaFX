3b39f6a761695ef373622d3f51bef1b5148351b9
Wiktor <wiktorkalinowski@gmail.com>
2016-11-22 18:02:30 +0100

    0.0.2
    Login Panel + New Acc Creator to SQL

--- BossReminder/src/sample/Main.java
+++ BossReminder/src/sample/Main.java
@@ -5,15 +5,38 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.sqlite.SQLiteConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class Main extends Application {
+    Connection conn;
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        PreparedStatement pst = null;
+
         Parent root = FXMLLoader.load(getClass().getResource("login1.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Login From SQLite Database ");
         primaryStage.setScene(new Scene(root, 255, 200));
         primaryStage.show();
+        CheckConnection();
+
+    }
+
+
+
+    public void CheckConnection() {
+     conn = Controller.DbConnector();
+         if ( conn == null) {
+            System.out.println("Connection Not Successful");
+            System.exit(1);
+        } else {
+            System.out.println("Connection Successful");
+        }
 
     }
 
